4b379e08fd476a624f4de6a3ae4cec32607a2df8
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-04-30 11:00:47 -0400

    address some warnings

--- clang/test/Cilk/rangelooptest.cpp
+++ clang/test/Cilk/rangelooptest.cpp
@@ -79688,43 +79688,43 @@ int Cilk_for_range_tests(int n) {
   std::vector<int> v(n);
   for (int i = 0; i < n; i++) v[i] = i;
 
-  _Cilk_for(auto x : v); // expected-warning {{Cilk for loop has empty body}}
-  _Cilk_for(auto& x : v); // expected-warning {{Cilk for loop has empty body}}
-  _Cilk_for(int x : v); // expected-warning {{Cilk for loop has empty body}}
-  _Cilk_for(std::string x : v); // expected-error {{no viable conversion from 'int' to 'std::string'}}
-  _Cilk_for(std::string x : {"a", "b"}); // expected-warning {{range-based for loop has empty body}}
+  _Cilk_for(auto x : v); // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
+  _Cilk_for(auto& x : v); // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
+  _Cilk_for(int x : v); // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
+  _Cilk_for(std::string x : v); // expected-error {{no viable conversion from 'int' to 'std::string'}} expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
+  _Cilk_for(std::string x : {"a", "b"}); // expected-warning {{range-based for loop has empty body}} expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
 
   // Pairs are aggregate types, which initially had a bug. Assert that they work
   std::vector<std::pair<int,int>> vp(n);
   for (int i = 0; i < n; i++) {
     vp[i] = std::make_pair(i,i+1);
   }
-  _Cilk_for(auto p : vp)
+  _Cilk_for(auto p : vp) // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     continue;
-  _Cilk_for(auto& p : vp) {
+  _Cilk_for(auto& p : vp) { // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     continue;
   }
 
   int a[5];
-  _Cilk_for(int x : a)
+  _Cilk_for(int x : a) //  expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
       continue;
 
   std::set<long> s(v.begin(), v.end());
-  _Cilk_for(int x : s); // expected-error {{Cannot determine length with '__end - __begin'. Please use a random access iterator.}}
+  _Cilk_for(int x : s); // expected-error {{Cannot determine length with '__end - __begin'. Please use a random access iterator.}} expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
   std::unordered_set<long> us(v.begin(), v.end());
-  _Cilk_for(int x : us); // expected-error {{Cannot determine length with '__end - __begin'. Please use a random access iterator.}}
+  _Cilk_for(int x : us); // expected-error {{Cannot determine length with '__end - __begin'. Please use a random access iterator.}} expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
 
   // Check for return statements, which cannot appear anywhere in the body of a
   // _Cilk_for loop.
-  _Cilk_for (int i : v) return 7; // expected-error{{cannot return}}
-  _Cilk_for (int i : v)
+  _Cilk_for (int i : v) return 7; // expected-error{{cannot return}} expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
+  _Cilk_for (int i : v) // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     for (int j = 1; j < i; ++j)
       return 7; // expected-error{{cannot return}}
 
   // Check for illegal break statements, which cannot bind to the scope of a
   // _Cilk_for loop, but can bind to loops nested within.
-  _Cilk_for (int i : v) break; // expected-error{{cannot break}}
-  _Cilk_for (int i : v)
+  _Cilk_for (int i : v) break; // expected-error{{cannot break}} expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
+  _Cilk_for (int i : v) // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     for (int j = 1; j < i; ++j)
       break;
 
@@ -79736,20 +79736,20 @@ int range_pragma_tests(int n) {
   for (int i = 0; i < n; i++) v[i] = i;
 
 #pragma clang loop unroll_count(4)
-  _Cilk_for (auto i : v)
+  _Cilk_for (auto i : v) // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     foo(i);
 
 #pragma cilk grainsize(4)
-  _Cilk_for (int i : v)
+  _Cilk_for (int i : v) // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     foo(i);
 
 #pragma cilk grainsize 4
-  _Cilk_for (auto i : v)
+  _Cilk_for (auto i : v) // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     foo(i);
 
 #pragma cilk grainsize = 4 \
 // expected-warning{{'#pragma cilk grainsize' no longer requires '='}}
-  _Cilk_for (int i : v)
+  _Cilk_for (int i : v) // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     foo(i);
 
   return 0;
@@ -79759,7 +79759,7 @@ int range_scope_tests(int n) {
   std::vector<int> v(n);
   for (int i = 0; i < n; i++) v[i] = i;
   int A[5];
-  _Cilk_for(int i : v) {
+  _Cilk_for(int i : v) { // expected-warning {{'_Cilk_for' support for for-range loops is currently EXPERIMENTAL only!}}
     int A[5];
     A[i%5] = i;
   }
