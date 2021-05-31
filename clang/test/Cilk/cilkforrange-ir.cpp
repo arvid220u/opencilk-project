284d98027c6ea6fca88daac48d2ff4a7ee3829d6
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-05-31 17:06:53 -0400

    ir fix

--- clang/test/Cilk/cilkforrange-ir.cpp
+++ clang/test/Cilk/cilkforrange-ir.cpp
@@ -1,5 +1,10 @@
 // RUN: %clang_cc1 %s -std=c++11 -triple x86_64-unknown-linux-gnu -fcilkplus -ftapir=none -verify -S -emit-llvm -o - | FileCheck %s
 // expected-no-diagnostics
+//
+// useful command:
+//    ./clang++ -std=c++11 -fopencilk -ftapir=none -S -emit-llvm ../opencilk-project/clang/test/Cilk/cilkforrange-ir.cpp
+//    cat cilkforrange-ir.ll | grep Z2upN1 -C 50
+
 
 namespace X {
 struct C {
@@ -26,64 +31,56 @@ void iterate(X::C c) {
     bar(x);
 }
 
-// TODO: why does the @_ZN1X1C3endEv call return void? it feels like this IR is wrong
-//  but it also seems like the IR for the corresponding normal for-range is wrong...
-
 // CHECK-LABEL: define void @_Z2iterateN1X1CE(
 
 // CHECK: %c = alloca %"struct.X::C", align 1
 // CHECK-NEXT: %syncreg = call token @llvm.syncregion.start()
 // CHECK-NEXT: %__range1 = alloca %"struct.X::C"*, align 8
-// CHECK-NEXT: %__begin1 = alloca %"struct.X::C::It", align 4
+// CHECK-NEXT: %[[BEGIN:.+]] = alloca %"struct.X::C::It", align 4
 // CHECK-NEXT: %__end1 = alloca %"struct.X::C::It", align 4
 // CHECK-NEXT: %__cilk_loopindex = alloca i32, align 4
 // CHECK-NEXT: %__cilk_looplimit = alloca i32, align 4
 // CHECK-NEXT: store %"struct.X::C"* %c, %"struct.X::C"** %__range1, align 8
 // CHECK-NEXT: %0 = load %"struct.X::C"*, %"struct.X::C"** %__range1, align 8
 // CHECK-NEXT: %call = call i32 @_ZN1X1C5beginEv(%"struct.X::C"* %0)
-// CHECK-NEXT: %coerce.dive = getelementptr inbounds %"struct.X::C::It", %"struct.X::C::It"* %__begin1, i32 0, i32 0
+// CHECK-NEXT: %coerce.dive = getelementptr inbounds %"struct.X::C::It", %"struct.X::C::It"* %[[BEGIN]], i32 0, i32 0
 // CHECK-NEXT: store i32 %call, i32* %coerce.dive, align 4
 // CHECK-NEXT: %1 = load %"struct.X::C"*, %"struct.X::C"** %__range1, align 8
 // CHECK-NEXT: %call1 = call i32 @_ZN1X1C3endEv(%"struct.X::C"* %1)
 // CHECK-NEXT: %coerce.dive2 = getelementptr inbounds %"struct.X::C::It", %"struct.X::C::It"* %__end1, i32 0, i32 0
 // CHECK-NEXT: store i32 %call1, i32* %coerce.dive2, align 4
 // CHECK-NEXT: store i32 0, i32* %__cilk_loopindex, align 4
-// CHECK-NEXT: %call3 = call i32 @_ZN1X1C2ItmiERS1_(%"struct.X::C::It"* %__end1, %"struct.X::C::It"* dereferenceable(1) %__begin1)
+// CHECK-NEXT: %call3 = call i32 @_ZN1X1C2ItmiERS1_(%"struct.X::C::It"* %__end1, %"struct.X::C::It"* dereferenceable(1) %[[BEGIN]])
 // CHECK-NEXT: store i32 %call3, i32* %__cilk_looplimit, align 4
 // CHECK-NEXT: br label %[[PFORCOND:.+]]
 
-// TODO: continue with the IR checks :))
-//  useful commands:
-//    ./clang++ -std=c++11 -fopencilk -ftapir=none -S -emit-llvm ../opencilk-project/clang/test/Cilk/cilkforrange-ir.cpp
-//    cat cilkforrange-ir.ll | grep Z2upN1 -C 50
+// CHECK: [[PFORCOND]]:
+// CHECK-NEXT: br label %[[PFORDETACH:.+]]
 
-// CHECK: [[PFORPH]]:
-// CHECK-NEXT: store i64 0, i64* %[[BEGIN:.+]], align 8
-// CHECK-NEXT: %[[ENDLIMIT:.+]] = load i64, i64* %[[LIMIT]]
-// CHECK-NEXT: %[[ENDINIT:.+]] = load i64, i64* %[[INIT]]
-// CHECK-NEXT: %[[ENDSUB:.+]] = sub i64 %[[ENDLIMIT]], %[[ENDINIT]]
-// CHECK-NEXT: %[[ENDSUB1:.+]] = sub i64 %[[ENDSUB]], 1
-// CHECK-NEXT: %[[ENDDIV:.+]] = udiv i64 %[[ENDSUB1]], 1
-// CHECK-NEXT: %[[ENDADD:.+]] = add i64 %[[ENDDIV]], 1
-// CHECK-NEXT: store i64 %[[ENDADD]], i64* %[[END:.+]], align 8
-
-// CHECK: %[[INITITER:.+]] = load i64, i64* %[[INIT]]
-// CHECK-NEXT: %[[BEGINITER:.+]] = load i64, i64* %[[BEGIN]]
-// CHECK-NEXT: %[[ITERMUL:.+]] = mul i64 %[[BEGINITER]], 1
-// CHECK-NEXT: %[[ITERADD:.+]] = add i64 %[[INITITER]], %[[ITERMUL]]
+// CHECK: [[PFORDETACH]]:
+// CHECK-NEXT: %[[INITITER:.+]] = load i32, i32* %__cilk_loopindex, align 4
 // CHECK-NEXT: detach within %[[SYNCREG:.+]], label %[[DETACHED:.+]], label %[[PFORINC:.+]]
 
 // CHECK: [[DETACHED]]:
-// CHECK: %[[ITERSLOT:.+]] = alloca i64, align 8
-// CHECK: store i64 %[[ITERADD]], i64* %[[ITERSLOT]]
+// CHECK-NEXT: %__local_loopindex = alloca i32, align 4
+// CHECK-NEXT: %[[X:.+]] = alloca i32, align 4
+// CHECK-NEXT: %[[ITER:.+]] = alloca %"struct.X::C::It", align 4
+// CHECK-NEXT: store i32 %[[INITITER]], i32* %__local_loopindex, align 4
+// CHECK-NEXT: %[[LOOPINDEXCOPY:.+]] = load i32, i32* %__local_loopindex, align 4
+// CHECK-NEXT: %[[ITERREF:.+]] = call i32 @_ZN1X1C2ItplEi(%"struct.X::C::It"* %[[BEGIN]], i32 [[LOOPINDEXCOPY]])
+// CHECK-NEXT: %[[ITER2:.+]] = getelementptr inbounds %"struct.X::C::It", %"struct.X::C::It"* %[[ITER]], i32 0, i32 0
+// CHECK-NEXT: store i32 %[[ITERREF]], i32* %[[ITER2]], align 4
+// CHECK-NEXT: %[[ELEM:.+]] = call dereferenceable(4) i32* @_ZN1X1C2ItdeEv(%"struct.X::C::It"* %[[ITER]])
+// CHECK-NEXT: %[[ELEMVAL:.+]] = load i32, i32* %[[ELEM]], align 4
+// CHECK-NEXT: store i32 %[[ELEMVAL]], i32* %[[X]], align 4
 
 // CHECK: [[PFORINC]]:
-// CHECK-NEXT: %[[INCBEGIN:.+]] = load i64, i64* %[[BEGIN]]
-// CHECK-NEXT: %[[INC:.+]] = add i64 %[[INCBEGIN]], 1
-// CHECK-NEXT: store i64 %[[INC]], i64* %[[BEGIN]]
-// CHECK-NEXT: %[[CONDBEGIN:.+]] = load i64, i64* %[[BEGIN]]
-// CHECK-NEXT: %[[CONDEND:.+]] = load i64, i64* %[[END]]
-// CHECK-NEXT: %[[COND:.+]] = icmp ult i64 %[[CONDBEGIN]], %[[CONDEND]]
+// CHECK-NEXT: %[[INCBEGIN:.+]] = load i32, i32* %__cilk_loopindex, align 4
+// CHECK-NEXT: %[[INC:.+]] = add nsw i32 %[[INCBEGIN]], 1
+// CHECK-NEXT: store i32 %[[INC]], i32* %__cilk_loopindex, align 4
+// CHECK-NEXT: %[[CONDBEGIN:.+]] = load i32, i32* %__cilk_loopindex, align 4
+// CHECK-NEXT: %[[CONDEND:.+]] = load i32, i32* %__cilk_looplimit, align 4
+// CHECK-NEXT: %[[COND:.+]] = icmp ne i32 %[[CONDBEGIN]], %[[CONDEND]]
 // CHECK-NEXT: br i1 %[[COND]], label %{{.+}}, label %[[PFORCONDCLEANUP:.+]], !llvm.loop ![[LOOPMD:.+]]
 
 // CHECK: [[PFORCONDCLEANUP]]:
